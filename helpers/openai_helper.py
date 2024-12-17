d92d0cb5433bf74adb034e5cc45a2811b30bee14
Fabian Tenzara <fabiantenzara@gmail.com>
2024-12-17 15:00:04 +0700

    refac: limit openai chatbot, it can only explain or response about forex, commodities, or index trading

--- helpers/openai_helper.py
+++ helpers/openai_helper.py
@@ -5,15 +5,30 @@ import json
 
 openai.api_key = OPENAI_API_KEY
 
+def validate_input(user_input):
+    """
+    Validate user input realted to trading topics
+    """
+    keywords = ['forex', 'index', 'commodities', 'stocks', 'trading', 'markets', 'gold', 'silver', 'oil', 'currency', 'indices']
+    input_to_lower = user_input.lower()
+    return any(keyword in input_to_lower for keyword in keywords)
+
 def generate_responses(user_input, temperature=0.7, max_tokens=50):
     """
     LLM CHATBOT
     """
     try:
+        if not validate_input(user_input):
+            return "I am specialized in trading-related queries. Please ask a question related to trading topics such as Forex, Indices, and Commodities."
+        
         messages = [
             {
                 "role": "system", 
-                "content": "You are a helpful assistant."
+                "content": (
+                    "You are a helpful assistant specialized in financial & trading markets. "
+                    "You can only provide responses related to trading topics such as Forex, Indices, and Commodities. "
+                    "If a user asks about other topics, politely decline to respond and remind them that you are specialized in trading-related queries. "
+                    )
             },
             {
                 "role": "user", 
